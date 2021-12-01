c93b79c5b695e9cd880fc851ccdd1d32637b9230
SeiichiN <billie175@gmail.com>
2021-12-01 16:19:18 +0900

    Dec.01 after

---
+++ ex6/src/Main.java
@@ -0,0 +1,11 @@
+import comment.Zenhan;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    Zenhan.doWarusa();
+    Zenhan.doTogame();
+    comment.Kouhan.callDeae();
+    comment.Kouhan.showMondokoro();
+  }
+
+}
