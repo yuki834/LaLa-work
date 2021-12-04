3464b32eeb9a7b80478e6917133efe83a95532ca
SeiichiN <billie175@gmail.com>
2021-12-04 20:33:59 +0900

    問題を追加

--- sukkiri/src/world/Item.java
+++ sukkiri/src/world/Item.java
@@ -4,10 +4,6 @@ public class Item {
 	public String name;
 	public int price;
 	
-	public Item() {
-		this.name = "ITEM";
-		this.price = 0;
-	}
 	
 	public Item(String name) {
 		this.name = name;
--- sukkiri/src/world/Weapon.java
+++ sukkiri/src/world/Weapon.java
@@ -1,12 +1,7 @@
 package world;
 
 public class Weapon extends Item {
-	public Weapon() {}
-	
-	public Weapon(String name) {
-		super(name);
-	}
-	public Weapon(String name, int price) {
-		super(name, price);
+	public Weapon() {
+		super("バズーカ");
 	}
 }
