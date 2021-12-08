f22256b4cc28f6197b88fe9f078eba8fb623ecd7
SeiichiN <billie175@gmail.com>
2021-12-08 21:11:02 +0900

    localDateTime added.

---
+++ sukkiri/src/chap15/Main15_14.java
@@ -0,0 +1,33 @@
+package chap15;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class Main15_14 {
+
+	public static void main(String[] args) {
+		
+		Instant i1 = Instant.now();
+		Instant i2 = Instant.ofEpochMilli(1600705425827L);
+		long l = i2.toEpochMilli();
+		
+		ZonedDateTime z1 = ZonedDateTime.now();
+		ZonedDateTime z2 = ZonedDateTime.of
+				(2020, 1, 2, 3, 4, 5, 6, ZoneId.of("Asia/Tokyo"));
+		
+		Instant i3 = z2.toInstant();
+		ZonedDateTime z3 = i3.atZone(ZoneId.of("Europe/London"));
+		
+		System.out.println("東京:" +
+		   z2.getYear() + z2.getMonth() + z2.getDayOfMonth());
+		
+		System.out.println("ロンドン:" +
+				   z3.getYear() + z3.getMonth() + z3.getDayOfMonth());
+		
+		if (z2.isEqual(z3)) {
+			System.out.println("これらは同じ瞬間を指しています");
+		}
+	}
+
+}
