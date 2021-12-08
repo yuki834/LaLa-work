afa8e5a156ee552405704f400fa743a603d35f82
SeiichiN <billie175@gmail.com>
2021-12-08 19:21:29 +0900

    練習問題13

---
+++ sukkiri/src/ex13/Wand.java
@@ -0,0 +1,27 @@
+package ex13;
+
+public class Wand {
+	private String name;
+	private double power;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		if (name.length() < 3) {
+			throw new IllegalArgumentException();
+		}
+		this.name = name;
+	}
+	public double getPower() {
+		return power;
+	}
+	public void setPower(double power) {
+		if (power < 0.5 || power > 100) {
+			throw new IllegalArgumentException();
+		}
+		this.power = power;
+	}
+	
+	
+}	
---
+++ sukkiri/src/ex13/Wizard.java
@@ -0,0 +1,61 @@
+package ex13;
+
+import world.Hero;
+
+public class Wizard {
+	private int hp;
+	private int mp;
+	private String name;
+	private Wand wand;
+	
+	public void heal(Hero h) {
+		int basePoint = 10;
+		int recovPoint = (int) (basePoint * this.wand.getPower());
+		h.setHp(h.getHp() + recovPoint);
+		System.out.println(h.getName() + "のHPを" + recovPoint + "回復した");
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		if (hp < 0) {
+			hp = 0;
+		}
+		this.hp = hp;
+	}
+
+	public int getMp() {
+		return mp;
+	}
+
+	public void setMp(int mp) {
+		if (mp < 0) {
+			throw new IllegalArgumentException();
+		}
+		this.mp = mp;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		if (name.length() < 3) {
+			throw new IllegalArgumentException();
+		}
+		this.name = name;
+	}
+
+	public Wand getWand() {
+		return wand;
+	}
+
+	public void setWand(Wand wand) {
+		if (wand == null ) {
+			throw new IllegalArgumentException();
+		}
+		this.wand = wand;
+	}
+}
